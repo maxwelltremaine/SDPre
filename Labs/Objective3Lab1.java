fe24f62e09291de86480bfccdcca26bc1452e3b5
Max <maxtremaine@aol.com>
2023-09-28 19:58:40 -0700

    finish Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,9 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1 = 67, num2 =47, num3 =27, average = (num1+num2+num3)/3;
+
+      System.out.println(average);
+
+  }
+}
+
