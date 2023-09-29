79b989f5f737042f7c7d1838ed1759c167ea5ed4
Max <maxtremaine@aol.com>
2023-09-28 20:59:13 -0700

    finish Objective3Lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,10 @@
+public class Objective3Lab3 {
+	public static void main(String[] args) {
+		int first = 1 + (int)(Math.random() * 6), second = 1 + (int)(Math.random() * 6), third = 1 + (int)(Math.random() * 6), sum = (first + second + third);
+		System.out.println(first); 
+		System.out.println(second); 
+		System.out.println(third); 
+		System.out.println(sum);
+	
+	}
+}
