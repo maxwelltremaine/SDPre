15383afc426acb835a43198ce4a1972b66890538
Max <maxtremaine@aol.com>
2023-09-28 17:40:33 -0700

    finish Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Maxwell Tremaine";
+    int age = 26;
+    String state = "Colorado";
+
+       System.out.println(name);
+        System.out.println(age);
+         System.out.println(state);
+    
+  }
+}
