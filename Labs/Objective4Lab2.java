6f0950e1e80b733961c3adfd45eaedccde270d8d
Max <maxtremaine@aol.com>
2023-09-30 15:52:11 -0700

    finish

---
+++ Labs/Objective4Lab2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Objective4Lab2 {
+  public static void main(String[] args) {
+
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("Please enter the first whole number you would like to add.");
+    int num1 = keyboard.nextInt();
+    System.out.println("Please enter the second whole number you would like to add.");
+    int num2 = keyboard.nextInt();
+    System.out.println("Please enter the third whole number you would like to add.");
+    int num3 = keyboard.nextInt();
+    System.out.println("Please enter the first decimal number you would like to add.");
+    double dub1 = keyboard.nextDouble();
+    System.out.println("Please enter the second decimal number you would like to add.");
+    double dub2 = keyboard.nextDouble();
+    System.out.println("Please enter the third decimal number you would like to add.");
+    double dub3 = keyboard.nextDouble();
+    int sum1 = num1 + num2 + num3;
+    System.out.println(" The sum of " + num1 + " and " + num2 + " and " + num3 + " is " + sum1 + ".");
+    double sum2 = dub1 + dub2 + dub3;
+    System.out.println(" The sum of " + dub1 + " and " + dub2 + " and " + dub3 + " is " + sum2 + ".");
+
+
+    
+
+    //TODO prompt the user to get input for all of the above int variables.
+
+    //TODO prompt the user to get input for all of the above double variables.
+
+    //TODO print the three ints and their sum.
+
+    //TODO print the three doubles and their sum.
+
+  }
+}
